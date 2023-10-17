2264b4d9247aabbdf103b3e76cd69bba8fb90d6c
Spencer Weaver <spencer@compassfoundation.io>
2023-10-17 13:25:05 -0400

    added account and server apps

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -30,6 +30,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External
+    "drf_spectacular",
+    "rest_framework",
+    # Internal
+    "account",
+    "server",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +120,19 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Pr',
+    "DESCRIPTION": "",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+}
