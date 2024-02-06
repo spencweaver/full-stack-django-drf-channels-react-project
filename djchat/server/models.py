5e940f00c6765d588af96f3697cd6bc63ff67b73
Spencer Weaver <spencweaver@gmail.com>
2024-02-06 17:46:48 -0500

    delete deprecated settings

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
@@ -11,7 +12,9 @@ class Category(models.Model):
 
 class Server(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner"
+    )
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="server_category")
     description = models.CharField(max_length=255, blank=True, null=True)
     member = models.ManyToManyField(settings.AUTH_USER_MODEL)
@@ -19,14 +22,18 @@ class Server(models.Model):
     def __str__(self):
         return self.name
 
+
 class Channel(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
+    )
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
 
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
         super(Channel, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.name
